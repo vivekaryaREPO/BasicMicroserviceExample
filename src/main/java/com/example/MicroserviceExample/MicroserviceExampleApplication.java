21a08a0ce241a0bac87aa577faf46dfef2b71aec
Vivek Arya <aryanss.orisiss@gmail.com>
2024-12-29 22:39:44 +0000

    first commit

---
+++ src/main/java/com/example/MicroserviceExample/MicroserviceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroserviceExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MicroserviceExample/MicroserviceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroserviceExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
